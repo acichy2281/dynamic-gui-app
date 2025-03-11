13545d77ada4785d8c6a93cf1cd500f35171a4a7
czehner1027 <ckm3320@wmich.edu>
2025-03-11 18:05:01 -0400

    Added stub code for property list request and reply

--- property_gatherer/library/inc/property_gatherer_messages.h
+++ property_gatherer/library/inc/property_gatherer_messages.h
@@ -58,6 +58,30 @@ namespace PropertyGatherer
         uint16_t messageId;
     };
 
+    struct PropertyDescriptor_T
+    {
+        // Property Descriptor Length
+        uint16_t propertyId;
+        // Property Flags
+        PropertyStorageVariantType_E propertyType;
+        // PropertyLength
+        std::string propertyName;
+        // Property Units
+    };
+
+    struct PropertyListRequest_T
+    {
+        Header_T header;
+    };
+
+    struct PropertyListReply_T
+    {
+        Header_T header;
+        uint16_t numProperties;
+        std::vector<PropertyDescriptor_T>
+        uint16_t status;
+    };
+
     struct GetValueReq_T
     {
         struct Header_T header;
@@ -73,6 +97,11 @@ namespace PropertyGatherer
         uint16_t status;
     };
 
+    PropertyListRequest_T GetPropertyListRequest();
+    PropertyListReply_T GetPropertyListReply();
+    GetValueReq_T GetValueRequest();
+    GetValueReply_T GetValueReply();
+
     class PropertyGathererMessageSerializer
     {
         public:
@@ -80,16 +109,21 @@ namespace PropertyGatherer
             ~PropertyGathererMessageSerializer();
 
             /* Serialization functions */
+            uint16_t Serialize(PropertyListRequest_T pMessage, std::vector<uint8_t>& outBuff);
+            uint16_t Serialize(PropertyListReply_T pMessage, std::vector<uint8_t>& outBuff);
             uint16_t Serialize(GetValueReq_T& pMessage, std::vector<uint8_t>& outBuff);
             uint16_t Serialize(GetValueReply_T& pMessage, std::vector<uint8_t>& outBuff);
 
             /* Deserialization functions */
+            bool Deserialize(PropertyListRequest_T& pMessage, std::vector<uint8_t>& msgBuff);
+            bool Deserialize(PropertyListReply_T& pMessage, std::vector<uint8_t>& msgBuff);
             bool Deserialize(GetValueReq_T& pMessage, std::vector<uint8_t>& msgBuf);
             bool Deserialize(GetValueReply_T& pMessage, std::vector<uint8_t>& msgBuf);
 
         private:
             void SerializeHeader(Header_T& header, std::vector<uint8_t>& outBuff);
             void DeserializeHeader(Header_T& header, std::vector<uint8_t>& msgBuf);
+
             uint16_t SerializeVariant(const PropertyStorageVariant& variant, std::vector<uint8_t>& outBuf);
             PropertyStorageVariant DeserializeVariant(std::vector<uint8_t>& inBuff, uint16_t& index);
     };
