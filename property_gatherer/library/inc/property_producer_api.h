abb1fdd5d48779f62fb4f4aa1718c16b6f4c93b7
czehner1027 <ckm3320@wmich.edu>
2025-03-11 19:13:34 -0400

    Added stub code for message handling for property producer

--- property_gatherer/library/inc/property_producer_api.h
+++ property_gatherer/library/inc/property_producer_api.h
@@ -10,21 +10,47 @@
 #include <memory>
 #include <inttypes.h>
 
+/* Shared Includes */
+#include "thread_safe_queue.h"
+
 /* Project Includes */
 #include "property_gatherer_messages.h"
 
 namespace PropertyGatherer
 {
+    enum class PropertyProducerState_E
+    {
+        INITIALIZED,
+        PROPERTY_LIST_POPULATED,
+        PROPERTY_LIST_REPLY_SENT,
+    };
+
     class PropertyProducer_C
     {
         public:
             PropertyProducer_C();
             ~PropertyProducer_C();
 
+            void PropertyProducer_ProcessReceivedMessage(std::unique_ptr<char[]>& msg, uint16_t size);
+            void PropertyProducer_ProcessTimedActivities();
+
+            /* Callbacks */
             virtual void PropertyProducer_OnPropertyListRequestRecieved() = 0;
             virtual void PropertyProducer_OnPropertyGetValueRequestRecieved() = 0;
 
         private:
+            uint16_t GetCurrentTimeMs();
+            void ProcessStateMachine();
+            void ProcessReceivedMessageQueue();
+            void ProcessReceivedPropertyListRequest();
+            void ProcessReceivedPropertyGetValueRequest(Message_T& msg);
+
+
+            /* Member variables */
+            PropertyProducerState_E _state = PropertyProducerState_E::INITIALIZED;
+            ThreadSafeQueue_C<Message_T> _msgQueue;
+            PropertyGathererMessageSerializer _msgSerializer;
+            std::_In_place_key_extract_map<uint32_t PropertyDescriptor_T> _propertyMap;
 
     };
 }
